63ba8d51b55fc19e91dd9d12295fa7741eeb411d
LMiguelx <luis.quispe.h@tecsup.edu.pe>
2024-10-16 19:39:06 -0500

    Lab11

---
+++ HanccoSnapchat/IniciarSesionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  HanccoSnapchat
+//
+//  Created by Pedro Josue Ponce Lopez on 16/10/24.
+//
+
+import UIKit
+import FirebaseAuth
+
+class IniciarSesionViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in print("Intentando Iniciar Sesion")
+            if error != nil{
+                print("Se presento el siguiente error: \(error)")
+            }else{
+                print("Inicio de sesion exitoso")
+            }
+        }
+    }
+    
+
+
+}
+
